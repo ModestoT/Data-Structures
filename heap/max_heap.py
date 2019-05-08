37db910ede78d12311920795d96c81288d044c40
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-05-08 16:17:59 -0500

    completed the bubble up method along with the insert method

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,29 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+
+    self._bubble_up(len(self.storage)-1)
 
   def delete(self):
     pass
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
     pass
 
   def _bubble_up(self, index):
-    pass
+    while index > 0:
+      parent = (index - 1) // 2
+
+      if self.storage[parent] < self.storage[index]:
+        self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+
+        index = parent
+      else:
+        break
 
   def _sift_down(self, index):
     pass
